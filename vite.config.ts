84e89df19670c544f886ebe26f5ef800546e8ed8
jashu <chamakurijashwanth@gmal.com>
2025-11-07 05:46:52 +0530

    fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/Portfolio/',
   server: {
     port: 5173, // optional — set your dev port
     open: true, // auto-opens browser on dev start
